4be734690849c24335e0bd230305ef0d41ae27ce
Andrey Chichinov <sysdos286@yahoo.com>
2017-12-16 00:54:00 -0500

    test for filter

--- test_version.cpp
+++ test_version.cpp
@@ -4,10 +4,27 @@
 
 BOOST_AUTO_TEST_CASE(split)
 {
-    BOOST_CHECK_EQUAL(1, 1);
+    auto result = otus::split("", '.');
+    BOOST_CHECK_EQUAL(1, result.size());
+    BOOST_CHECK(result[0].empty());
+
+    result = otus::split("11", '.');
+    BOOST_CHECK_EQUAL(1, result.size());
+    BOOST_CHECK_EQUAL("11", result[0]);
+
+    result = otus::split("..", '.');
+    BOOST_CHECK_EQUAL(3, result.size());
+    for (const auto& r : result) {
+        BOOST_CHECK(r.empty());
+    }
 }
 
 BOOST_AUTO_TEST_CASE(filter)
 {
-    BOOST_CHECK_EQUAL(1, 1);
+    std::vector<std::vector<std::string>> pool { 
+        {"", "", "", ""}, {"1", "70", "44", "170"}
+    };
+
+    
+    BOOST_CHECK_EQUAL(2, pool.size());
 }
