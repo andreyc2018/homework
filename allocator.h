031049f9ee63954767037e0e5de0955442dac2f0
Andrey Chichinov <sysdos286@yahoo.com>
2017-12-24 11:48:00 -0500

    Implemented custome allocator

--- allocator.h
+++ allocator.h
@@ -2,64 +2,94 @@
 
 #include <iostream>
 #include <memory>
+#include <map>
 
-template <typename T, int N>
-struct hw_allocator
+namespace otus {
+
+extern void* LatestHead;
+
+template <typename T, size_t N>
+class hw_allocator
 {
-    using value_type = T;
+    public:
 
-    template<typename U>
-    struct rebind
-    { using other = hw_allocator<U, N>; };
+        using value_type = T;
 
-    size_t i;
-    const size_t max_elements;
+        template<typename U>
+        struct rebind
+        { using other = hw_allocator<U, N>; };
 
-    T* head_;
+        hw_allocator()
+            : i_(0), max_elements_(N), head_(nullptr), copy_(false) {}
 
-    hw_allocator() : i(0), max_elements(N), head_(nullptr)
-    {
-        std::cerr << "ctor: hw_allocator() i = " << i
-                  << " max = " << max_elements << "\n";
-    }
+        hw_allocator(const hw_allocator<T, N>& other)
+            : i_(other.i())
+            , max_elements_(other.max_elements())
+            , head_(other.head())
+            , copy_(true) {}
 
-    ~hw_allocator()
-    {
-        std::cerr << "dtor: ~hw_allocator() i = " << i
-                  << " max = " << max_elements
-                  << " head = " << head_ << "\n";
-        free(head_);
-    }
+// error: cannot convert ‘std::_Rb_tree_node<std::pair<const int, int> >*’ to ‘std::pair<const int, int>*’ in initialization
+//        template <class U>
+//        hw_allocator(const hw_allocator<U, N>& other)
+//            : i_(other.i())
+//            , max_elements_(other.max_elements())
+//            , head_(other.head())
+//            , copy_(true) {}
 
-    T* allocate(std::size_t n)
-    {
-        if (head_ == nullptr) {
-            head_ = static_cast<T*>(malloc(max_elements * sizeof (T)));
+// error: invalid static_cast from type ‘const hw_allocator<std::_Rb_tree_node<std::pair<const int, int> >, 10>’ to type ‘hw_allocator<std::pair<const int, int>, 10>&’
+//        template <class U>
+//        hw_allocator(const hw_allocator<U, N>& other)
+//            : i_(static_cast<hw_allocator<T, N>&>(other).i())
+//            , max_elements_(static_cast<hw_allocator<T, N>&>(other).max_elements())
+//            , head_(static_cast<hw_allocator<T, N>&>(other).head())
+//            , copy_(true) {}
+
+// error: no type named ‘allocator_type’ in ‘struct std::_Rb_tree_node<std::pair<const int, int>>’
+//        template <class U>
+//        hw_allocator(const hw_allocator<U, N>& other)
+//            : i_(static_cast<typename U::allocator_type&>(other).i())
+//            , max_elements_(static_cast<typename U::allocator_type&>(other).max_elements())
+//            , head_(static_cast<typename U::allocator_type&>(other).head())
+//            , copy_(true) {}
+
+        ~hw_allocator() { if (!copy_) free(head_); }
+
+        T* allocate(std::size_t n)
+        {
+            if (head_ == nullptr) {
+                head_ = static_cast<T*>(malloc(max_elements_ * sizeof (T)));
+                LatestHead = head_;
+                std::cout << "head = " << head_
+                          << " latest = " << LatestHead << "\n";
+            }
+            T* ptr = &(head_[i_]);
+            i_ += n;
+            return ptr;
         }
-        std::cerr << "i = " << i << " max = " << max_elements
-                  << " head = " << head_ << "\n";
-        T* ptr = &(head_[i++]);
-        std::cerr << "allocate  : " << n << " " << ptr
-                  << " ptr - head = " << ptr - head_ << "\n";
-        return ptr;
-    }
 
-    void deallocate(T* ptr, std::size_t n)
-    {
-        std::cerr << "deallocate: " << n << " " << ptr << "\n";
-    }
+        void deallocate(T*, std::size_t) {}
 
-    template <typename U, typename... Args>
-    void construct(U* ptr, Args&&... args)
-    {
-        std::cerr << "construct : " << ptr << " with " << sizeof...(args) << " args\n";
-        ::new(static_cast<void*>(ptr)) U(std::forward<Args>(args)...);
-    }
+        template <typename U, typename... Args>
+        void construct(U* ptr, Args&&... args)
+        {
+            std::cout << "sizeof(U) = " << sizeof (U)
+                      << " sizeof(T) = " << sizeof (T) << "\n";
 
-    template <typename U>
-    void destroy(U* ptr)
-    {
-        std::cerr << "destroy   : " << ptr << "\n";
-        ptr->~U();
-    }
+            ::new(static_cast<void*>(ptr)) U(std::forward<Args>(args)...);
+        }
+
+        template <typename U>
+        void destroy(U* ptr) { ptr->~U(); }
+
+        size_t i() const { return i_; }
+        size_t max_elements() const { return max_elements_; }
+        T* head() const { return head_; }
+
+    private:
+        size_t i_;
+        const size_t max_elements_;
+        T* head_;
+        bool copy_;
 };
+
+}
