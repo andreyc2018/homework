6b2a8e83799232ebe1707f1ff703c78e817fbd82
Andrey Chichinov <sysdos286@yahoo.com>
2017-12-31 16:37:14 -0500

    cleanup master branch

--- main.cpp
+++ main.cpp
@@ -1,24 +1,10 @@
-#include "ip_filter.h"
 #include <iostream>
 
 int main(int, char const**)
 {
-    try
-    {
-        otus::pool_t ip_pool;
-
-        for(std::string line; std::getline(std::cin, line);) {
-            auto v = otus::split(line, '\t');
-            ip_pool.push_back(otus::split(v.at(0), '.'));
-        }
-
-        otus::print_reverse(std::cout, ip_pool);
-        otus::print_filter(std::cout, ip_pool, 46);
-        otus::print_filter(std::cout, ip_pool, 46, 70);
-        otus::print_filter_any(std::cout, ip_pool, 46);
+    try {
     }
-    catch(const std::exception &e)
-    {
+    catch(const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
 
