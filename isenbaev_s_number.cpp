5a33a76f2ea3acbe92f218eca5316afd42262c33
Andrey Chichinov <ac01@achome.net>
2018-02-12 06:32:41 -0500

    task #4: wip

--- isenbaev_s_number.cpp
+++ isenbaev_s_number.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using graph = std::vector<std::vector<std::string>>;
 
 int main(int, char const**)
 {
     try {
+        size_t n;
+        std::string line;
+        std::getline(std::cin, line);
+        n = std::stoul(line);
+        for (size_t i = 0; i < n; ++i) {
+            std::getline(std::cin, line);
+            std::cout << line << "\n";
+        }
     }
     catch(const std::exception &e) {
         std::cerr << e.what() << std::endl;
