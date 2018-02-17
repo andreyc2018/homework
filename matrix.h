94a0358a499b750ea624e250792cddceca0fcfd7
Andrey Chichinov <ac01@achome.net>
2018-02-16 20:16:30 -0500

    wip

--- matrix.h
+++ matrix.h
@@ -2,26 +2,34 @@
 
 #include <set>
 #include <array>
+#include <cstdint>
 
 namespace matrix
 {
 
-template<typename T, uint Dimension>
+/**
+ */
+template<typename T, int Dimension>
 class Cell
 {
     public:
-        Cell();
-        ~Cell();
-
         /// variadic set allows to set
         template<typename... Args>
-        void set(T value, Args... dimensions)
+        void set(T v, Args&&... args)
         {
-
+            std::array<int, Dimension> coords = { { args... } };
+            coordinates_.swap(coords);
+            value_ = v;
         }
 
+        T get() const { return value_; }
+
+        bool operator< (const Cell& other) const
+        {
+            return coordinates_ == other.coordinates_ && value_ < other.value_;
+        }
     private:
-        std::array<uint, Dimension> coordinates_;
+        std::array<uint8_t, Dimension> coordinates_;
         T value_;
 };
 
--- matrix_test.cpp
+++ matrix_test.cpp
@@ -3,8 +3,13 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
+using matrix::Cell;
+
 BOOST_AUTO_TEST_CASE(init)
 {
+    Cell<int, 2> c1;
+    c1.set(10, 1, 1);
+    
     BOOST_CHECK_EQUAL(1, 0);
     BOOST_CHECK(false);
 }
