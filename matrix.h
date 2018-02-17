3e95f00d93ba1b0b031d56f6c0d7d19f029d4d26
Andrey Chichinov <sysdos286@yahoo.com>
2018-02-16 23:00:47 -0500

    in the middle to move to separate set_v and set_c

--- matrix.h
+++ matrix.h
@@ -2,45 +2,86 @@
 
 #include <set>
 #include <array>
-#include <cstdint>
 
 namespace matrix
 {
 
 /**
+ * @brief The Cell class
+ *
+ * Represents a cell in a matrix of dimension Dimension
+ * Keeps it's own coordinates and a value
  */
-template<typename T, int Dimension>
+template<typename T, size_t Dimension = 2>
 class Cell
 {
     public:
-        /// variadic set allows to set
-        template<typename... Args>
-        void set(T v, Args&&... args)
+        using value_t = T;
+        using cell_t = Cell<T, Dimension>;
+
+        Cell() = default;
+
+        template<typename... Args,
+                 typename std::enable_if_t<Dimension == sizeof...(Args), size_t> = 0>
+        Cell(Args&&... args) : Cell(T(), std::forward<Args>(args)...) {}
+
+        template<typename... Args,
+                 typename std::enable_if_t<Dimension == sizeof...(Args), size_t> = 0>
+        Cell(value_t v, Args&&... args)
         {
-            std::array<int, Dimension> coords = { { args... } };
-            coordinates_.swap(coords);
-            value_ = v;
+            set_value(v);
+            set_coordinates(std::forward<Args>(args)...);
         }
 
-        T get() const { return value_; }
+        template<typename... Args,
+                 typename std::enable_if_t<Dimension == sizeof...(Args), size_t> = 0>
+        void set_coordinates(Args&&... args)
+        {
+            std::array<size_t, Dimension> coords = { { args... } };
+            coordinates_.swap(coords);
+        }
+
+        cell_t& operator=(value_t v) noexcept { set_value(v); return *this; }
+
+        void set_value(value_t v) { value_ = v; }
+        T get_value() const { return value_; }
 
         bool operator< (const Cell& other) const
         {
-            return coordinates_ == other.coordinates_ && value_ < other.value_;
+            return coordinates_ < other.coordinates_;
         }
+
     private:
-        std::array<uint8_t, Dimension> coordinates_;
-        T value_;
+        std::array<size_t, Dimension> coordinates_;
+        value_t value_;
 };
 
-template<typename T, int Dimension, int Default>
+template<typename T, size_t Dimension, T Default>
 class Matrix
 {
     public:
+        using value_t = T;
+        using cell_t = Cell<T, Dimension>;
+
         Matrix();
         ~Matrix();
 
+        template<typename... Args>
+        typename std::enable_if_t<Dimension == sizeof...(Args), value_t>&
+        operator[](Args&&... args)
+        {
+//            return mVector[idx];
+        }
+
+        template<typename... Args>
+        typename std::enable_if_t<Dimension == sizeof...(Args), const value_t>&
+        operator[](Args&&... args) const
+        {
+//            return mVector[idx];
+        }
+
     private:
+        std::set<cell_t> matrix_;
 };
 
 }
--- matrix_test.cpp
+++ matrix_test.cpp
@@ -7,10 +7,81 @@ using matrix::Cell;
 
 BOOST_AUTO_TEST_CASE(init)
 {
-    Cell<int, 2> c1;
-    c1.set(10, 1, 1);
-    
-    BOOST_CHECK_EQUAL(1, 0);
-    BOOST_CHECK(false);
+    Cell<int> c1(1, 1);
+    c1.set_value(10);
+
+    Cell<int> c2(1, 1);
+    c2.set_value(10);
+
+// error: no matching function for call to ‘matrix::Cell<int, 3>::set(int, int, int)’
+//    constexpr int Dimension = 3;
+//    Cell<int, 3> c3;
+//    int value = 11;
+//    int x = 1, y = 1, z = 0;
+//    c3.set(value, x, y); Number of coordinates is less than a dimension
+
+    BOOST_CHECK(!(c1 < c2));
+
+    Cell<int> c4;
+    c4.set(12, 0, 0);
+    BOOST_CHECK(c4 < c1);
 }
 
+BOOST_AUTO_TEST_CASE(add_to_set)
+{
+    constexpr int Dimension = 2;
+    using cell_t = Cell<int, Dimension>;
+    std::set<cell_t> matrix;
+
+    cell_t c1;
+    c1.set(10, 1, 1);
+
+    cell_t c2;
+    c2.set(11, 1, 1);
+
+    auto rc = matrix.insert(c1);
+    BOOST_CHECK(rc.second);
+    BOOST_CHECK_EQUAL(1, matrix.size());
+    BOOST_CHECK_EQUAL(c1.get(), rc.first->get());
+
+    rc = matrix.insert(c2);
+    BOOST_CHECK(!rc.second);
+    BOOST_CHECK_EQUAL(1, matrix.size());
+    BOOST_CHECK_EQUAL(c1.get(), rc.first->get());
+
+    cell_t c3(1, 1);
+    BOOST_CHECK_EQUAL(0, c3.get());
+    auto it = matrix.find(c3);
+    BOOST_CHECK_EQUAL(c1.get(), it->get());
+}
+
+BOOST_AUTO_TEST_CASE(assignment)
+{
+    constexpr int Dimension = 2;
+    using cell_t = Cell<int, Dimension>;
+
+    std::set<cell_t> matrix;
+
+    cell_t c4(1, 2);
+    auto it = matrix.find(c4);
+    BOOST_CHECK(it == matrix.end());
+
+    c4.set(12);
+    auto rc = matrix.insert(c4);
+    BOOST_CHECK(rc.second);
+    BOOST_CHECK_EQUAL(c4.get(), rc.first->get());
+
+    it = matrix.find(c4);
+    BOOST_CHECK(it != matrix.end());
+    BOOST_CHECK_EQUAL(c4.get(), it->get());
+
+    cell_t c5(3, 3);
+    c5 = 12;
+    rc = matrix.insert(c5);
+    BOOST_CHECK(rc.second);
+    BOOST_CHECK_EQUAL(c5.get(), rc.first->get());
+
+    it = matrix.find(c5);
+    BOOST_CHECK(it != matrix.end());
+    BOOST_CHECK_EQUAL(c5.get(), it->get());
+}
