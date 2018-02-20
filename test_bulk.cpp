b9140db38811739624af520281a41d2ded7bade0
Andrey Chichinov <ac01@achome.net>
2018-02-20 08:12:41 -0500

    create 7.0

---
+++ test_bulk.cpp
@@ -0,0 +1,11 @@
+#include "bulk.h"
+#define BOOST_TEST_MODULE Bulk
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(init)
+{
+    BOOST_CHECK_EQUAL(1, 0);
+    BOOST_CHECK(false);
+}
+
