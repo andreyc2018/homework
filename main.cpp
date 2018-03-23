a814b9cdce25db6816b8b047c5e603f543e1de91
Andrey Chichinov <ac01@achome.net>
2018-03-22 20:08:30 -0400

    add threads [skip ci]

--- main.cpp
+++ main.cpp
@@ -3,6 +3,33 @@
 #include <iostream>
 #include <string>
 
+void console(MsgQueue& q)
+{
+    std::string data;
+    do
+    {
+        data = q.pop();
+        if (!data.empty()) {
+            std::cout << data;
+        }
+    } while (!data.empty());
+}
+
+void remote_file(MsgQueue& q)
+{
+    std::string data;
+    do
+    {
+        std::string filename = q.pop();
+        data = q.pop();
+        if (!data.empty()) {
+            std::ofstream file(filename);
+            file << data;
+            file.close();
+        }
+    } while (!data.empty());
+}
+
 int main(int argc, char const** argv)
 {
     try {
@@ -14,12 +41,23 @@ int main(int argc, char const** argv)
         }
         int n = std::stoi(argv[1]);
 
+        MsgQueue console_q;
+        std::thread console_thread(console, std::ref(console_q));
+        MsgQueue file_q;
+        std::thread file_thread_1(console, std::ref(file_q));
+        std::thread file_thread_2(console, std::ref(file_q));
+
+
         Processor processor(n,
-                            std::make_unique<LocalWriterFactory>());
+                            std::make_unique<ThreadWriterFactory>(console_q, file_q));
         for (std::string input; std::getline(std::cin, input);) {
             processor.add_token(input);
         }
         processor.end_of_stream();
+
+        console_thread.join();
+        file_thread_1.join();
+        file_thread_2.join();
     }
     catch(const std::exception &e) {
         std::cerr << e.what() << std::endl;
