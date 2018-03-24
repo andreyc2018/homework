84822cf8c4198829d6a3f5769e89ee5998736021
Andrey Chichinov <sysdos286@yahoo.com>
2018-03-24 00:52:14 -0400

    Add logger. Implement main for async_cli

--- async.cpp
+++ async.cpp
@@ -1,15 +1,23 @@
 #include "async.h"
+#include "logger.h"
 
 namespace async {
 
-handle_t connect(std::size_t bulk) {
+handle_t connect(std::size_t bulk)
+{
+    gLogger->debug("{}: size = {}", __func__, bulk);
     return nullptr;
 }
 
-void receive(handle_t handle, const char *data, std::size_t size) {
+void receive(handle_t handle, const char *data, std::size_t size)
+{
+    gLogger->debug("{}: handle = {}, data = {}, size = {}",
+                   __func__, handle, data, size);
 }
 
-void disconnect(handle_t handle) {
+void disconnect(handle_t handle)
+{
+    gLogger->debug("{}: handle = {}", __func__, handle);
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
-#include "processor.h"
+#include "async.h"
+#include "logger.h"
 #include <iostream>
 #include <string>
 
 int main(int argc, char const** argv)
 {
+    try {
+        if (argc < 2) {
+            std::cout << "usage: "
+                      << std::string(argv[0]).substr(std::string(argv[0]).rfind("/")+1)
+                      << " <N>\nwhere:\n  N - command block size\n";
+            exit(1);
+        }
+
+        if (argc > 2) {
+            gLogger->set_level(spdlog::level::debug);
+        }
+
+        std::size_t bulk = std::stoul(argv[1]);
+        auto h = async::connect(bulk);
+        auto h2 = async::connect(bulk);
+        async::receive(h, "1", 1);
+        async::receive(h2, "1\n", 2);
+        async::receive(h, "\n2\n3\n4\n5\n6\n{\na\n", 15);
+        async::receive(h, "b\nc\nd\n}\n89\n", 11);
+        async::disconnect(h);
+        async::disconnect(h2);
+    }
+    catch(const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
+
+/*
     try {
         if (argc < 2) {
             std::cout << "usage: "
@@ -22,5 +52,4 @@ int main(int argc, char const** argv)
         std::cerr << e.what() << std::endl;
     }
 
-    return 0;
-}
+*/
