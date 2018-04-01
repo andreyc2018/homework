547be64511faa76784f952537960e7d389998be3
Andrey Chichinov <ac01@achome.net>
2018-04-01 00:24:40 -0400

    accept empty string. get rid of raw pointers. delete unused file.

--- parser.cpp
+++ parser.cpp
@@ -3,58 +3,53 @@
 #include "processor.h"
 #include "logger.h"
 
-Parser::Parser(Processor* processor)
+Parser::Parser(Processor& processor)
     : dynamic_level_(0)
     , state_(new StartingBlock)
     , processor_(processor)
     , open_kw_(std::make_shared<term_t>("\\{"))
     , close_kw_(std::make_shared<term_t>("\\}"))
-    , command_(std::make_shared<term_t>("[^{}]+"))
+    , command_(std::make_shared<term_t>("^$|[^{}]+"))
 {
 }
 
 Parser::~Parser()
 {
-    delete state_;
 }
 
 void Parser::handle_token(const std::string& token)
 {
-//    LOG() << "Processing: " << token << "\n";
-    while (state_->handle(this, token) == ParserState::Result::Continue) {}
+    while (state_->handle(*this, token) == ParserState::Result::Continue) {}
 }
 
 void Parser::end_of_stream()
 {
-    if (state_->end_of_stream(this) == ParserState::Result::Continue) {
-        while (state_->handle(this, "") == ParserState::Result::Continue) {}
+    if (state_->end_of_stream(*this) == ParserState::Result::Continue) {
+        while (state_->handle(*this, "") == ParserState::Result::Continue) {}
     }
 }
 
-void Parser::set_state(ParserState* state)
+void Parser::set_state(ParserStateUPtr&& state)
 {
-//    LOG() << "from state: " << state_->name();
-    delete state_;
-    state_ = state;
-//    LOG() << " to state: " << state_->name() << "\n";
+    state_.swap(state);
 }
 
 void Parser::add_command(const std::string& token)
 {
-    processor_->add_command(token);
+    processor_.add_command(token);
 }
 
 bool Parser::block_complete() const
 {
-    return processor_->block_complete();
+    return processor_.block_complete();
 }
 
 void Parser::start_block()
 {
-    processor_->start_block();
+    processor_.start_block();
 }
 
 void Parser::notify_run()
 {
-    processor_->run();
+    processor_.run();
 }
