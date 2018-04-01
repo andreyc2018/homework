e7b781cc7913ea2462ec6d80da61f6d8add67f34
Andrey Chichinov <ac01@achome.net>
2018-04-01 11:41:55 -0400

    code cleanup

--- main.cpp
+++ main.cpp
@@ -32,24 +32,3 @@ int main(int argc, char const** argv)
     }
     return 0;
 }
-
-/*
-    try {
-        if (argc < 2) {
-            std::cout << "usage: "
-                      << std::string(argv[0]).substr(std::string(argv[0]).rfind("/")+1)
-                      << " <N>\nwhere:\n  N - command block size\n";
-            exit(1);
-        }
-        int n = std::stoi(argv[1]);
-        Processor processor(n);
-        for (std::string input; std::getline(std::cin, input);) {
-            processor.add_token(input);
-        }
-        processor.end_of_stream();
-    }
-    catch(const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-
-*/
