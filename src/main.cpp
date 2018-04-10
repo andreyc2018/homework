2adf8d57076f3226eece0f351039b0d83829e9bb
Andrey Chichinov <ac01@achome.net>
2018-04-10 19:51:36 -0400

    adding asio [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,135 @@
 #include "async.h"
 #include "logger.h"
+#include <boost/asio.hpp>
+
 #include <iostream>
 #include <string>
 #include <thread>
+
+using boost::asio::ip::tcp;
+
+class chat_session
+  : public chat_participant,
+    public std::enable_shared_from_this<chat_session>
+{
+public:
+  chat_session(tcp::socket socket, chat_room& room)
+    : socket_(std::move(socket)),
+      room_(room)
+  {
+  }
+
+  void start()
+  {
+    room_.join(shared_from_this());
+    do_read_header();
+  }
+
+  void deliver(const chat_message& msg)
+  {
+    bool write_in_progress = !write_msgs_.empty();
+    write_msgs_.push_back(msg);
+    if (!write_in_progress)
+    {
+      do_write();
+    }
+  }
+
+private:
+  void do_read_header()
+  {
+    auto self(shared_from_this());
+    boost::asio::async_read(socket_,
+        boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec && read_msg_.decode_header())
+          {
+            do_read_body();
+          }
+          else
+          {
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+
+  void do_read_body()
+  {
+    auto self(shared_from_this());
+    boost::asio::async_read(socket_,
+        boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            room_.deliver(read_msg_);
+            do_read_header();
+          }
+          else
+          {
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+
+  void do_write()
+  {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_,
+        boost::asio::buffer(write_msgs_.front().data(),
+          write_msgs_.front().length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            write_msgs_.pop_front();
+            if (!write_msgs_.empty())
+            {
+              do_write();
+            }
+          }
+          else
+          {
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  chat_room& room_;
+  chat_message read_msg_;
+  chat_message_queue write_msgs_;
+};
+
+class BulkServer
+{
+    public:
+        BulkServer(boost::asio::io_service& io_service, const tcp::endpoint& endpoint)
+            : acceptor_(io_service, endpoint)
+            , socket_(io_service)
+        {
+            do_accept();
+        }
+
+    private:
+        void do_accept()
+        {
+            acceptor_.async_accept(socket_,
+                                   [this](boost::system::error_code ec)
+            {
+                if (!ec)
+                {
+                    std::make_shared<chat_session>(std::move(socket_), room_)->start();
+                }
+
+                do_accept();
+            });
+        }
+
+        tcp::acceptor acceptor_;
+        tcp::socket socket_;
+};
 
 void full_session(int size)
 {
@@ -17,14 +144,17 @@ void full_session(int size)
 int main(int argc, char const** argv)
 {
     try {
-        if (argc < 2) {
+        if (argc < 3) {
             std::cout << "usage: "
                       << std::string(argv[0]).substr(std::string(argv[0]).rfind("/")+1)
-                      << " <N>\nwhere:\n  N - command block size\n";
+                      << " <port> <N>\n"
+                         "where:\n"
+                         "  port - tcp port for incomming connections\n"
+                         "  N    - command block size\n";
             exit(1);
         }
 
-        if (argc > 2) {
+        if (argc > 3) {
             gLogger->set_level(spdlog::level::debug);
         }
 
