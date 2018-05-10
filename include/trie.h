c1e6111b0650779b9ea74e287bfae9692c4dd6c4
Andrey Chichinov <sysdos286@yahoo.com>
2018-05-09 22:34:37 -0400

    wip [skip ci]

--- include/trie.h
+++ include/trie.h
@@ -7,10 +7,12 @@
 class TrieNode
 {
     public:
-        using type_t = char;
+        using container_t = std::string;
+        using type_t = container_t::value_type;
         using node_t = TrieNode;
         using children_t = std::map<type_t, node_t>;
         using iterator = children_t::iterator;
+        using const_iterator = children_t::const_iterator;
 
         TrieNode() {}
         TrieNode(TrieNode&& rv) = default;
@@ -19,14 +21,16 @@ class TrieNode
         TrieNode& operator=(TrieNode&& rv) = default;
         TrieNode& operator=(const TrieNode& v) = default;
 
-        iterator find(type_t v)
-        {
-        }
+        iterator find(type_t v) { return children_.find(v); }
+        const_iterator find(type_t v) const { return children_.find(v); }
 
-        bool operator==(const type_t& other) const { return value_ == other; }
-        bool operator!=(const type_t& other) const { return !operator==(other); }
-        bool operator==(const TrieNode& other) const { return operator==(other.value_); }
-        bool operator!=(const TrieNode& other) const { return !operator==(other.value_); }
+        iterator end() { return children_.end(); }
+        const_iterator end() const { return children_.end(); }
+
+        bool operator==(const type_t& r) const { return value_ == r; }
+        bool operator!=(const type_t& r) const { return !operator==(r); }
+        bool operator==(const TrieNode& r) const { return operator==(r.value_); }
+        bool operator!=(const TrieNode& r) const { return !operator==(r.value_); }
 
     private:
         type_t value_;
@@ -37,19 +41,24 @@ class Trie
 {
     public:
         using node_t = TrieNode;
+        using container_t = node_t::container_t;
+
         Trie() {}
 
-        T find(const std::string& /*data*/)
-        {
-        }
+//        T find(const std::string& /*data*/)
+//        {
+//        }
 
-        void add(const std::string& data)
+        void add(const container_t& data)
         {
             node_t& current = root_;
-            for (const auto& e : data) {
+            for (size_t i = 0; i < data.size(); ++i) {
+                auto& e = data[i];
                 std::cout << e << " ";
-                auto it = root_.find(e);
-                if (root_.contains(e)) {
+                if (current != e) {
+                    auto it = current.find(e);
+                    if (it != current.end()) {
+                    }
                 }
             }
             std::cout << '\n';
